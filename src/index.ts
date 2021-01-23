7ec89d704d79e1124437bc9187f4063de4651501
Seiko Santana <santanaaseiko@gmail.com>
2021-01-24 03:26:26 +0700

    Enhanced chart, update data on input change, auto focus on input field

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { Chart } from "./chart.js";
+import {Chart} from "./chart";
 import "./chart.js";
 
 let canvas: HTMLCanvasElement = document.querySelector("#forChart") as HTMLCanvasElement;
 // let context = canvas.getContext("2d");
 
-let chart: Chart = new Chart(canvas, {
+let mainChart: Chart = new Chart(canvas, {
     type: "line",
     data: {
         labels: [
@@ -33,6 +33,7 @@ let chart: Chart = new Chart(canvas, {
     },
     options: {
         responsive: true,
+        maintainAspectRatio: false,
         title: {
             text: "Sequence Visualization"
         },
@@ -42,16 +43,48 @@ let chart: Chart = new Chart(canvas, {
                   display: true, // mandatory
                   scaleLabel: {
                        display: true, // mandatory
-                       labelString: 'Your label' // optional 
+                       labelString: '' // optional 
                   },
+                  ticks: {
+                      min: 0
+                  }
              }], 
             yAxes: [{ // and your y axis customization as you see fit...
                display: true,
                scaleLabel: {
                     display: true,
-                    labelString: 'Count'
+                    labelString: 'Values'
+               },
+               ticks: {
+                   suggestedMin: 0
                }
            }],
        }
     }
+})
+
+let valuesField: HTMLInputElement = document.querySelector("#values") as HTMLInputElement;
+valuesField.addEventListener("input", () => {
+    setChartData(valuesField.value);
+})
+
+function setChartData(valueString: string) {
+    let valuesTrimmed = valueString.trim();
+    let values: Array<Chart.ChartPoint> = valuesTrimmed.split(" ").map((value, index) => {
+        return {
+            x: index,
+            y: parseFloat(value)
+        }
+    });
+    let trend: Array<Chart.ChartPoint> = [values[0], values[values.length - 1]];
+    mainChart.data.datasets![0].data = values;
+    mainChart.data.datasets![1].data = trend;
+    mainChart.update({
+        duration: 500,
+        easing: "easeInOutSine"
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    valuesField.focus();
 })
